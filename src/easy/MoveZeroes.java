c2230412661e37fbeb9f9e501db5e4cb20bfdae4
Nishant Banginwar <banginwar.nishant1@gmail.com>
2024-09-15 15:06:14 +0530

    4

---
+++ src/easy/MoveZeroes.java
@@ -0,0 +1,55 @@
+package easy;
+
+public class MoveZeroes {
+	public void moveZeroes(int[] nums) { // my sol
+		for (int i = nums.length - 1; i >= 0; i--) {
+			if (nums[i] == 0) {
+				for (int j = i; j < nums.length - 1; j++) {
+					int temp = nums[j];
+					nums[j] = nums[j + 1];
+					nums[j + 1] = temp;
+					// System.out.println(j);
+				}
+			}
+		}
+		for (int i = 0; i < nums.length; i++)
+			System.out.print(nums[i] + " ");
+		System.out.println();
+	}
+
+	public void moveZeroes1(int[] nums) { // O(N)
+		int k = 0; // track 0th position
+		for (int i = 0; i <nums.length; i++) {
+			if (nums[i] != 0) {
+				int temp = nums[k];
+				nums[k] = nums[i];
+				nums[i] = temp;
+				k++;
+			}
+		}
+		for(int i = 0; i < nums.length; i++)
+			System.out.print(nums[i] + " ");
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MoveZeroes mz = new MoveZeroes();
+		int[] A = { 0, 1, 0, 3, 12 };
+		int[] B = { 0 };
+		int[] C = { 0, 0, 0, 3, 12 };
+		int[] D = { 0, 4, 11, 0 };
+		int[] E = { 1, 2, 3, 0, 0 };
+		mz.moveZeroes(A); // 1 3 12 0 0 
+		mz.moveZeroes(B); // 0
+		mz.moveZeroes(C); // 3 12 0 0 0 
+		mz.moveZeroes(D); // 4 11 0 0 
+		mz.moveZeroes(E); // 1 2 3 0 0 
+		mz.moveZeroes1(new int[]{0, 1, 0, 3, 12} );
+		mz.moveZeroes1(new int[]{0});
+		mz.moveZeroes1(new int[]{0, 0, 0, 3, 12});
+		mz.moveZeroes1(new int[]{0, 4, 11, 0});
+		mz.moveZeroes1(new int[]{1, 2, 3, 0, 0});
+	}
+
+}
